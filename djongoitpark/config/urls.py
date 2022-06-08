6e6c37f1d682c49f17510d07824469dc4638c2dc
Kairat Sultan <productgsm@gmail.com>
2022-06-08 21:54:34 +0500

    media root changes

--- djongoitpark/config/urls.py
+++ djongoitpark/config/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('courses.urls'), name='main'),
     path('admin/', admin.site.urls, name='admin'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += [
