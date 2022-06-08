6d27617bf3b1bf5103556ee0ba8079c040bb8681
Kairat Sultan <productgsm@gmail.com>
2022-06-08 21:50:56 +0500

    admin preview changes

--- djongoitpark/courses/admin.py
+++ djongoitpark/courses/admin.py
@@ -1,16 +1,37 @@
 from django.contrib import admin
 from .models import Courses, Centers
+from django.utils.safestring import mark_safe
 
 
 class CoursesAdmin(admin.ModelAdmin):
-    pass
+    exclude = ('slug', 'image')
+
+    def image_preview(self, obj):
+        # ex. the name of column is "image"
+        if obj.image:
+            return mark_safe(
+                '<img src="{0}" width="150" height="150" style="object-fit:contain" />'.format(obj.image_file.url))
+        else:
+            return '(No image)'
+
+    image_preview.short_description = 'Preview'
 
 
 admin.site.register(Courses, CoursesAdmin)
 
 
 class CentersAdmin(admin.ModelAdmin):
-    pass
+    exclude = ('slug', 'image')
+
+    def image_preview(self, obj):
+        # ex. the name of column is "image"
+        if obj.image:
+            return mark_safe(
+                '<img src="{0}" width="150" height="150" style="object-fit:contain" />'.format(obj.image_file.url))
+        else:
+            return '(No image)'
+
+    image_preview.short_description = 'Preview'
 
 
 admin.site.register(Centers, CoursesAdmin)
